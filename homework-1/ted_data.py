86cbae8fd040ff6818c20dd1fad79fa0777d15f8
Sufian Latif <sufian.latif.bd@gmail.com>
2018-01-25 06:44:00 -0500

    Read the data from CSV file into TEDData object.

---
+++ homework-1/ted_data.py
@@ -0,0 +1,48 @@
+import csv
+import pprint
+import re
+
+
+class TEDData:
+    headers = []
+
+    def __init__(self, row):
+        self.comments = int(row[0])
+        self.desc = row[1]
+        self.duration = int(row[2])
+        self.event = row[3]
+        self.film_date = int(row[4])
+        self.languages = int(row[5])
+        self.main_speaker = row[6]
+        self.name = row[7]
+        self.num_speaker = row[8]
+        self.published_date = int(row[9])
+        self.ratings = eval(row[10])
+        self.related_talks = eval(row[11])
+        self.speaker_occupation = re.split('[/;]', row[12].lower())
+        self.tags = eval(row[13])
+        self.title = row[14]
+        self.url = row[15]
+        self.views = int(row[16])
+
+
+def read_data(file_name):
+    with open(file_name, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        TEDData.headers = next(reader)
+        data = []
+
+        for row in reader:
+            data.append(TEDData([s.strip() for s in row]))
+
+    return data
+
+
+def main():
+    data = read_data('ted_main.csv')
+    print(len(data))
+    pprint.pprint(data[0].__dict__)
+
+
+if __name__ == '__main__':
+    main()
