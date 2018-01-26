aa7af11d72995673a5f76e70caa035e286575470
Sufian Latif <sufian.latif.bd@gmail.com>
2018-01-25 20:26:42 -0500

    Function for finding the top tags.

--- homework-1/ted_data_analysis.py
+++ homework-1/ted_data_analysis.py
@@ -5,6 +5,7 @@ import numpy as np
 
 data = read_data('ted_main.csv')
 
+
 def duration_histogram():
     interval = 5
     d = [d.duration / 60.0 for d in data]
@@ -13,4 +14,30 @@ def duration_histogram():
     pyplot.axvline(np.mean(d), color='black')
     pyplot.show()
 
+
 # duration_histogram()
+
+def tag_groups():
+    tag_frequency = dict()
+    for d in data:
+        for tag in d.tags:
+            if tag in tag_frequency:
+                tag_frequency[tag] += 1
+            else:
+                tag_frequency[tag] = 1
+    # pprint([(k, tag_frequency[k]) for k in sorted(tag_frequency, key=tag_frequency.get, reverse=True)])
+
+    group_size = 50
+    tag_groups = dict()
+    for tag in tag_frequency:
+        gr = int(tag_frequency[tag] / group_size)
+        if gr * group_size in tag_groups:
+            tag_groups[gr * group_size].append(tag)
+        else:
+            tag_groups[gr * group_size] = [tag]
+
+
+    pprint(tag_groups)
+
+# tag_groups()
+
