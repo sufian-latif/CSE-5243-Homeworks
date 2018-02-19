2818ab4b580e872292e253ea1e56ec86c2014597
sufian002 <sufian.latif.bd@gmail.com>
2018-02-19 06:09:15 -0500

    Added statistics (confusion matrix, ROC curve) from k-NN results.

--- homework-2/wine_data_knn.py
+++ homework-2/wine_data_knn.py
@@ -29,3 +29,64 @@ def knn(k, dist):
     return results
 
 # pprint(knn(3, dist_euclid))
+
+
+def show_statistics(k, dist):
+    results = knn(k, dist)
+    tmp = [[r[0], r[1]] for r in results]
+    tp = tmp.count([WineData.HIGH, WineData.HIGH])
+    fn = tmp.count([WineData.HIGH, WineData.LOW])
+    fp = tmp.count([WineData.LOW, WineData.HIGH])
+    tn = tmp.count([WineData.LOW, WineData.LOW])
+
+    print('k = ' + str(k))
+    print('-' * 30)
+
+    print('Confusion Matrix')
+    print(' ' * 8 + 'Predicted')
+    print(' ' * 9 + '+' + ' ' * 5 + '-')
+    print(' ' * 7 + '-' * 11)
+    print(' ' * 5 + '+|' + '{0:4d} |{1:4d} |'.format(tp, fn))
+    print('Actual|' + '-' * 11 + '|')
+    print(' ' * 5 + '-|' + '{0:4d} |{1:4d} |'.format(fp, tn))
+    print(' ' * 7 + '-' * 11)
+
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f_measure = 2 * precision * recall / (precision + recall)
+
+    print('Precision = ' + str(precision))
+    print('Recall = ' + str(recall))
+    print('F-measure = ' + str(f_measure))
+
+    for r in results:
+        if r[1] == WineData.LOW:
+            r[2] = 1 - r[2]
+
+    roc_points_x = [0]
+    roc_points_y = [0]
+    tpr = 0
+    fpr = 0
+    np = [r[0] for r in results].count(WineData.HIGH)
+
+    for r in sorted(results, key=lambda x: x[-1], reverse=True):
+        if r[0] == WineData.HIGH:
+            tpr += 1 / np
+        else:
+            fpr += 1 / np
+
+        roc_points_x.append(fpr)
+        roc_points_y.append(tpr)
+
+    roc_points_x.append(1)
+    roc_points_y.append(1)
+
+    pyplot.plot(roc_points_x, roc_points_y)
+    pyplot.plot([0, 1], [0, 1])
+    pyplot.show()
+
+    print()
+    print()
+
+
+show_statistics(1, dist_euclid)
